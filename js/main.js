36c38126f2994939653565296a1c199c84b4b00d
pre-entrega <galasaucedo0@gmail.com>
2023-10-26 23:15:46 -0300

    agregue fecha de entrega, alerts y mensajes

--- js/main.js
+++ js/main.js
@@ -34,9 +34,6 @@ const categoriesEnable = categoriesDisponi(products)
 
 const filter = getCategoryName(categoriesEnable)
 
-console.log(filter)
-
-// alert(`las categorias disponibles son: ${filter.join(", ")}`)
 
 // despejo los titulos de los productos disponibles
 
@@ -46,28 +43,64 @@ const productTitle = categoriesEnable.map((product) => product.title
 // ordenos los productos de A a Z
 const productsAyZ = productTitle.sort()
 
-console.log(productTitle)
 
 const listProducts = productsAyZ.map((product, i)=>
 `${i + 1 } ${product} \n`  
-
 )
 
-// le pregunto al usuario que producto desea seleccionar ingresando el numero del producto
 
-let eleccion = prompt(`ingrese el numero del producto que desea comprar:\n ${listProducts}`)
+// Despejo el producto completo de la lista de productos disponibles 
+
+const electionDeProduct = () => {
+
+    
+    let election = productTitle[eleccion] 
+ 
+    let productoElegido 
+
+    products.map((product) => {
+ 
+    if (election === product.title) {
+ 
+     productoElegido = product
+     
+    }
+    }
+    )
+    // avanza con la compra seleccionando una opcion rambon de una fecha para entrega
+ 
+     const compra = confirm(`NOMBRE: ${productoElegido.title}\n DESCRIPCION: ${productoElegido.description}\n PRECIO: ${productoElegido.price}\n ¿DESEA AVANZAR CON LA COMPRA?`)
+
+
+     if (compra) {
+        let end = new Date()
+        let start = new Date(2023, 10, 26)
+        
+            let fechaEntrega = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+          
+         alert(`PERFECTO! EL PRODUCTO SERA ENTREGADO EN LA FECHA: ${fechaEntrega.toLocaleDateString(`es-AR`)}`)
+
+    } else {
+        alert("GRACIAS POR SU VISITA!")
+
+    } 
+
+} 
+
+// el usuario debe elegir el producto por el numero, si no ingresa el correcto la lista vuelve a aparecer con otro mensaje
+
+
+let eleccion = prompt(`INGRESE EL NUMERO DEL PRODUCTO QUE DESEE:\n ${listProducts}`)
+
+
 
 while (eleccion < 1 || eleccion > 10 ) { 
 
-     eleccion = prompt(`ingrese una opcion valida:\n ${listProducts}`)
-}
+     eleccion = prompt(`UPS! ESA OPCION NO ESTA EN LA LISTA, INGRESE UNA OPCION CORRECTA:\n ${listProducts}`)
+} if (productTitle) {
 
-const compra = productsAyZ [
-    eleccion - 1 
-]
+    eleccion = eleccion -1
 
-console.log(compra)
+    electionDeProduct()
 
-
-
-
+} 
